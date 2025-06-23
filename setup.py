c6103b9bed86791bc2ed1f75abff619855962f7d
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2025-06-23 06:19:28 -0700

    Update fork info

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name="pyshuf",
+    name="pyshuf2",
     version="0.0.1",
-    author="Jake Kara",
-    description="Inexact clone of GNU shuf. Shuffles order of lines in a file.",
+    author="Matthew Yang",
+    description="An attempt to bugfix an inexact python clone of GNU shuf and bring it into the modern Python 3 era.",
     license="GPL-3",
     keywords="shuf",
-    url="https://github.org/jakekara/pyshuf",
+    url="https://github.org/matthewyang204/pyshuf",
     repository="https://github.org/jakekara/pyshuf",
     packages=["pyshuf"],
     entry_points = {
-        "console_scripts": ["pyshuf=pyshuf.__main__:main"]
+        "console_scripts": ["pyshuf2=pyshuf2.__main__:main"]
     }
 )
