661c98e58ab714147451e205c94e4ef2e05a8af4
Matthew Yang <matthew.yang204@gmail.com>
2025-08-24 18:22:33 -0700

    Update setup.py metadata
    v0.0.1 -> v2.0.0
    set proper repos

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="pyshuf2",
-    version="0.0.1",
+    version="2.0.0",
     author="Matthew Yang",
     description="An attempt to bugfix an inexact python clone of GNU shuf and bring it into the modern Python 3 era.",
     license="GPL-3",
     keywords="shuf",
     url="https://github.org/matthewyang204/pyshuf",
-    repository="https://github.org/jakekara/pyshuf",
+    repository="https://github.org/matthewyang204/pyshuf",
     packages=["pyshuf2"],
     entry_points = {
         "console_scripts": ["pyshuf2=pyshuf2.__main__:main"]
